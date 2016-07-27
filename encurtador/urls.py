93d3500b06ddad51a2bab5ec383be958f32193c4
KleberMonte <kleberdarlanmonte@gmail.com>
2016-07-27 19:17:25 -0300

    Atualizando requirements

--- encurtador/urls.py
+++ encurtador/urls.py
@@ -4,6 +4,4 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include('urlapp.urls', namespace='urlapp')),
-    # url('^short/', include('shorturls.urls'))
-    # url(r'^weeny/', include("weeny.urls")),
 ]
