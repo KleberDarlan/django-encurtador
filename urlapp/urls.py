9470f65206d072345ecde8978ce28b97ab69ecea
KleberMonte <kleberdarlanmonte@gmail.com>
2016-07-27 19:51:28 -0300

    Ajustando url padrão

--- urlapp/urls.py
+++ urlapp/urls.py
@@ -4,7 +4,7 @@ from .views import UrlView, UrlRequestView, UrlCreateView
 
 
 urlpatterns = [
-    url(r'^home/$', UrlView.as_view(), name='home'),
+    url(r'^', UrlView.as_view(), name='home'),
     url(r'^(?P<id>\w+)$', UrlRequestView.as_view(), name='short'),
     url(r'^create/$', UrlCreateView.as_view(), name='create'),
 ]
