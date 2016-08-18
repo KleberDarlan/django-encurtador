ac418ce4e25bb13ee43e76450a686c671a15dbe4
KleberMonte <kleberdarlanmonte@gmail.com>
2016-08-18 14:44:25 -0300

    Ajustando url encurtada

--- encurtador/urls.py
+++ encurtador/urls.py
@@ -5,8 +5,8 @@ from urlapp.views import UrlView
 
 
 urlpatterns = [
-    # url(r'^$', lambda r: HttpResponseRedirect('home/')),
-    url(r'^$', UrlView.as_view(), name='home'),
-    url(r'^s/', include('urlapp.urls', namespace='urlapp')),
+    url(r'^$', lambda r: HttpResponseRedirect('home/')),
+    url('^', include('urlapp.urls', namespace='urlapp')),
+    url(r'^home/', UrlView.as_view(), name='home'),
     url(r'^admin/', admin.site.urls),
 ]
--- urlapp/signals.py
+++ urlapp/signals.py
@@ -5,8 +5,8 @@ def post_save_url(instance, created, **kwargs):
     from urlapp.models import Url
 
     if created:
-        instance.short_url = settings.BASE_URL + 's/' + short_url.encode_url(instance.short_id.clock_seq)
+        instance.short_url = settings.BASE_URL + short_url.encode_url(instance.short_id.clock_seq)
 
         while Url.objects.filter(short_url=instance.short_url).exists():
-            instance.short_url = settings.BASE_URL + 's/' + short_url.encode_url(uuid.uuid4().clock_seq)
+            instance.short_url = settings.BASE_URL + short_url.encode_url(uuid.uuid4().clock_seq)
         instance.save()
